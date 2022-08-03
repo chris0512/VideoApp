e2ef5bc2e0d851824fb5456dd5b9c45236c5ba65
chris0512 <kyang3200@gmail.com>
2022-08-03 17:21:10 +0800

    Initial Commit

---
+++ VideoPlayApp/VideoPlayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayAppApp.swift
+//  VideoPlayApp
+//
+//  Created by Yang Chi on 2022/8/3.
+//
+import SwiftUI
+
+@main
+struct VideoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
